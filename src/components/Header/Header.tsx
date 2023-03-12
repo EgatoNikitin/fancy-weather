69bce661dc3c3c545a22f33e531d57ca472ef234
alinapodrez <alinapodrez@gmail.com>
2023-03-12 20:28:32 +0300

    add searchForm

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,30 @@
 import { SeltctButton } from '../selectButton/selectButton'
 import { RefreshButton } from '../refreshButton/refreshButton'
+import { SearchForm } from '../SearchForm/SearchForm';
 
-import React, {FC} from 'react';
+import React, { FC, useState } from 'react';
 
 import './Header.scss';
 
+type HeaderProps = {
+    setImg: (value: string) => void,
+}
 
-export const Header: FC = () => {
+export const Header: FC<HeaderProps> = ({ setImg }) => {
+
+    const changeBackground = () => {
+
+    }
+
     return (
         <header className="header">
             <div className='controllers'>
-                <RefreshButton/>
-                <SeltctButton/>
+                <RefreshButton onClick={changeBackground} />
+                <SeltctButton />
                 <button className='button fah unactive'>°F</button>
                 <button className='button cel'>°C</button>
             </div>
+            <SearchForm />
         </header>
     );
 }
--- src/components/refreshButton/refreshButton.tsx
+++ src/components/refreshButton/refreshButton.tsx
@@ -2,7 +2,11 @@ import React, {FC} from 'react';
 import './refreshButton.scss';
 import reficon from '../../assets/img/vector.svg';
 
-export const RefreshButton: FC = () => {
+type RefreshButtonProps = {
+    onClick: ()=>void
+}
+
+export const RefreshButton: FC<RefreshButtonProps> = ({onClick}) => {
     return (
         <button type='button' className='RefreshButton'><img src={reficon} alt="#"/></button>
     );
